def15cf4da889f7052eb52744277aecb6b03e416
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2020-05-17 10:34:46 +0700

    Modified user controller

--- app/controllers/users_controller.py
+++ app/controllers/users_controller.py
@@ -3,8 +3,9 @@ Provide logic for /users endpoint
 """
 from sqlalchemy.orm import Session
 
-from app.schemas.users_schema import UsersAction
+from app.schemas.users_schema import UsersCreate, UsersUpdate
 from app.models.users_model import Users
+from app.utils import auth
 
 
 def get_users(sql: Session):
@@ -17,23 +18,40 @@ def get_user(sql: Session, user_id: int):
     return sql.query(Users).filter(Users.UserId == user_id).first()
 
 
-def login_user(sql: Session, username: str):
+def check_user(sql: Session, username: str):
     """return a user data"""
     user = sql.query(Users).filter(Users.Username == username).first()
     return user
 
 
-def create_user(sql: Session, user: UsersAction):
+def create_user(sql: Session, user: UsersCreate):
     """
-    Create a record of user with its Username, Email, Password
+    Create a record of user with its Username and Password
     Password will be hashed
     """
     new_user = Users(
-        Username=user.Name,
-        Email=user.Email,
-        Password=user.Password
+        Username=user.Username,
+        Password=auth.get_password_hash(user.Password)
     )
     sql.add(new_user)
     sql.commit()
     sql.refresh(new_user)
     return new_user
+
+
+def update_user(sql: Session, user_id: int, user: UsersUpdate):
+    """
+    Update a specific user data
+    """
+    old_data = sql.query(Users).filter(Users.UserId == user_id).first()
+
+    if old_data is not None:
+        new_data = user.dict()
+        old_data.Fullname = new_data["Fullname"]
+        old_data.Email = new_data["Email"]
+        old_data.Password = auth.get_password_hash(new_data["Password"])
+        old_data.Status = new_data["Status"]
+
+        sql.commit()
+
+    return old_data
